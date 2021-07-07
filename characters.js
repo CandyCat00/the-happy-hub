5565b603ecebda2e6d9ba62d7e253e011f89f681
Glitch (thimble-import) <none>
2021-07-07 18:08:38 +0000

    🚕❤️ Checkpoint ./characters.js:31179025/316

--- characters.js
+++ characters.js
@@ -2,28 +2,32 @@ let yasashi = {
   'image': 'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F598C326D-4529-46CA-BC60-723AEBC36847.jpeg?v=1625601300615',
   'name' : 'Yasashi',
   'basic': 'Species: Corgi<br />Coat: Blue<br />Eye Color: Yellow<br />Weight: 25lb<br />',
-  'background': 'Cheerful and upbeat, Yasashi’s first few years of her childhood was spent at “Puppy Pawz Orphange” after getting separated from her parents. Her blue fur made it hard for her to fit in, making her very self conscious about her look. However, when she is acquainted with others dear to her heart, she’s known to put their well being above her own. Her smile is quite contagious.'
+  'background': 'Cheerful and upbeat, Yasashi’s first few years of her childhood was spent at “Puppy Pawz Orphange” after getting separated from her parents. Her blue fur made it hard for her to fit in, making her very self conscious about her look. However, when she is acquainted with others dear to her heart, she’s known to put their well being above her own. Her smile is quite contagious.',
+  'gallery': []
 }
 
 let scarem = {
   'image': '',
   'name': 'Scarem',
   'basic': 'Species: Fruit Bat<br />Coat: Brown<br />Eye Color: Violet<br />',
-  'background': ''
+  'background': '',
+  'gallery': []
 }
 
 let ginji = {
   'image': '',
   'name' : 'Ginji',
   'basic': 'Species: Tanooki<br />Coat: Orange<br />Eye Color: Cyan<br />',
-  'background': ''
+  'background': '',
+  'gallery': []
 }
 
 let mantis = {
   'image': '',
   'name' : 'Mantis',
   'basic': 'Species: Unknown<br />Coat: White<br />Eye Color: Purple<br />',
-  'background': ''
+  'background': '',
+  'gallery': []
 }
 
 let displayYasashi = () => {
@@ -31,6 +35,7 @@ let displayYasashi = () => {
   document.querySelector('#name').innerHTML = yasashi.name;
   document.querySelector('#basic').innerHTML = yasashi.basic;
   document.querySelector('#background').innerHTML = yasashi.background;
+  fillGallery(yasashi.gallery);
 }
 
 document.querySelector('#yasashi').addEventListener('click', displayYasashi);
@@ -40,6 +45,7 @@ let displayScarem = () => {
   document.querySelector('#name').innerHTML = scarem.name;
   document.querySelector('#basic').innerHTML = scarem.basic;
   document.querySelector('#background').innerHTML = scarem.background;
+  fillGallery(scarem.gallery);
 }
 
 document.querySelector('#scarem').addEventListener('click', displayScarem);
@@ -49,6 +55,7 @@ let displayGinji = () => {
   document.querySelector('#name').innerHTML = ginji.name;
   document.querySelector('#basic').innerHTML = ginji.basic;
   document.querySelector('#background').innerHTML = ginji.background;
+  fillGallery(ginji.gallery);
 }
 
 document.querySelector('#ginji').addEventListener('click', displayGinji);
@@ -71,6 +78,7 @@ function fillGallery(gallery) {
     listItem = document.createElement('li');
     //assign each gallery image to new li
     listItem.innerHTML = gallery[i];
-    /
+    //add the new li to the gallery ul
+    document.querySelector('#gallery').appendChild(listItem);
   }
 }
