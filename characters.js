533f1f5a9e9e45b6e2c2a58d6c4c4e860c666b7c
Glitch (thimble-import) <none>
2021-07-07 16:39:20 +0000

    📟📖 Checkpoint ./characters.js:31179025/1301

--- characters.js
+++ characters.js
@@ -1,13 +1,23 @@
 let yasashi = {
-  
+  'image': 'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F598C326D-4529-46CA-BC60-723AEBC36847.jpeg?v=1625601300615',
+  'name' : 'Yasashi',
+  'basic': 'Species: Corgi<br />Coat: Blue<br />Eye Color: Yellow<br />Height: 10in<br />Weight: 25lb<br />',
+  'background': 'Cheerful and upbeat, Yasashi’s first few years of her childhood was spent at “Puppy Pawz Orphange” after getting separated from her parents. Her blue fur made it hard for her to fit in, making her very self conscious about her look. However, when she is acquainted with others dear to her heart, she’s known to put their well being above her own. Her smile is quite contagious.'
 }
 
 let scarem = {
-  
+  'image': '',
+  'name': 'Scarem',
+  'basic': 'Species: Fruit Bat<br />Coat: Brown<br /> Eye Color: Violet<br />',
+  'background': ''
 }
 
 let ginji = {
-  
+  'image': '',
+  'name' : 'Ginji',
+  'basic': 'Species: Tanooki<br />Coat: Re'
 }
 
-let mantis
+let mantis = {
+  
+}
