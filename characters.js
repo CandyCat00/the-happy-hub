d4ab278522a5312bafa3838741717ad6981bc6e1
Glitch (thimble-import) <none>
2021-07-08 00:02:05 +0000

    🔦🚜 Checkpoint ./characters.js:31179025/2317

--- characters.js
+++ characters.js
@@ -5,9 +5,12 @@ let yasashi = {
   'background': 'Cheerful and upbeat, Yasashi’s first few years of her childhood was spent at “Puppy Pawz Orphange” after getting separated from her parents. Her blue fur made it hard for her to fit in, making her very self conscious about her look. However, when she is acquainted with others dear to her heart, she’s known to put their well being above her own. Her smile is quite contagious.',
   'gallery': ['https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F610FB785-5AA9-4A42-839A-6C98040C2AD4.jpeg?v=1625700101830',
               'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F7D8702D8-FAB2-4A43-9BD8-98BB40443E70.jpeg?v=1625700112254',
-              'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2FBC9D490B-AE24-46D9-9862-2DEC0BE6F89C.jpeg?v=1625700125918',
               'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F8CB0AC3B-C69B-4C5D-A568-CB54B90423BD.jpeg?v=1625700155251',
-              '']
+              'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F1D4C7856-80C9-45EA-A48D-B2426EA92802.jpeg?v=1625700215121',
+              'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2FD429B2BA-082B-4CA9-9213-EE4FC8D31C1B.jpeg?v=1625700220217',
+              'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F425C92C7-7AA3-4EED-B02B-51452AECF09E.jpeg?v=1625700225633',
+              'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2FA507992B-C631-47E3-9730-9E753C0386D5.jpeg?v=1625700239250',
+              'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F9FAFCA2E-D2FA-43DB-B0A3-E4CDD485E95A.jpeg?v=1625700247757']
 }
 
 let scarem = {
@@ -18,7 +21,10 @@ let scarem = {
   'gallery': ['https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F5F2B9735-3A6F-4D04-8296-001B72B878CA.jpeg?v=1625700131735',
               'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F1553A53E-1CA5-487F-A6E6-2FCD89B6E558.jpeg?v=1625700145065',
               'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F449FF295-77F6-4339-9FB5-0264266E5681.jpeg?v=1625700149812',
-              '']
+              'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F593A6B6D-5D45-4185-BF81-34AD28C4FE7A.jpeg?v=1625700188552',
+              'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F77C9B424-FC4F-42AF-94FD-4D071A0068E8.jpeg?v=1625700195981',
+              'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F2470D2E1-8949-4B31-88C1-0427650BC49E.jpeg?v=1625700203216',
+              'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F3EA296C0-F513-4AD0-BCDF-9641E08FC9B9.jpeg?v=1625700078558']
 }
 
 let ginji = {
@@ -26,14 +32,14 @@ let ginji = {
   'name' : 'Ginji',
   'basic': 'Species: Tanooki<br />Coat: Orange<br />Eye Color: Cyan<br />',
   'background': 'Genji grew up on a farm with his parents. Being quite poor and having to continuously give their savings away to taxes for the kingdom, Genji made it his goal to do whatever it takes to earn money for his family. Sometimes it causes to commit betrayal to people he gains trust with. This causes him to make lots of enemies and little friends. Though he doesn’t seem to care as all he feels he needs is himself and his family. That is until Mantis crashed into his life…',
-  'gallery': ['https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2FprettyBoi.jpeg?v=1624753111273',
-              'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F958DD1BE-4B68-4C4E-B4A2-8CF584C9CD81.jpeg?v=1625700075044',
+  'gallery': ['https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F958DD1BE-4B68-4C4E-B4A2-8CF584C9CD81.jpeg?v=1625700075044',
               'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2FA458EC4E-9461-418D-AB57-F3A670D56A66.jpeg?v=1625700086938',
               'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2FEEA82A6F-F18E-4801-998B-7B93681F7665.jpeg?v=1625700092523',
               'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F610FB785-5AA9-4A42-839A-6C98040C2AD4.jpeg?v=1625700101830',
               'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F7D8702D8-FAB2-4A43-9BD8-98BB40443E70.jpeg?v=1625700112254',
               'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2F491F4D3D-C600-4150-B904-DBB4AB576C05.jpeg?v=1625700162301',
-              'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2FE95EF549-F54A-455F-B9DB-356FCF188374.jpeg?v=1625700183605']
+              'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2FE95EF549-F54A-455F-B9DB-356FCF188374.jpeg?v=1625700183605',
+              'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2FprettyBoi.jpeg?v=1624753111273']
 }
 
 let mantis = {
@@ -47,6 +53,8 @@ let mantis = {
               'https://cdn.glitch.com/9302465f-36ff-4dbd-8f13-95adddccdfd2%2FA53E54FD-C519-45DA-9BEA-91B4B76216F8.jpeg?v=1625700174161']
 }
 
+
+//functions to change the HTML content when the character changes
 let displayYasashi = () => {
   document.querySelector('#image').src = yasashi.image;
   document.querySelector('#name').innerHTML = yasashi.name;
